c6ec8f1c76f7b282cc3672a2773e1e540f6cb2d0
shikoryak <oksanashykoriak@gmail.com>
2023-06-16 16:12:54 +0300

    add files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-'use strict';
+const buttonMenu = document.getElementById('buttonMenu');
+const headerNavbar = document.getElementById('headerNavbar');
+
+buttonMenu.onclick = function() {
+  headerNavbar.classList.add('open');
+};
+
+headerNavbar.onclick = function() {
+  headerNavbar.classList.remove('open');
+};
